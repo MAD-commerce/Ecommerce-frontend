ca961e3863a7d7869f4c1454f89cd21a403087e2
Andres Rios <andreciito38@gmail.com>
2024-11-12 23:07:06 -0500

    conf

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  assetsInclude: ['**/*.png', '**/*.jpg'],
   build: {
     outDir: 'dist', // Asegúrate de que el directorio de salida esté configurado
   },
